878af748b7ca7ed33877b4c679f1c3d7b6420612
freedomwebtech <48402744+freedomwebtech@users.noreply.github.com>
2025-05-26 14:27:28 +0530

    Update test.py

--- test.py
+++ test.py
@@ -6,18 +6,9 @@ model = YOLO('best.pt')
 names = model.names
 
 
-# Open video
-cap = cv2.VideoCapture("people.mp4")
-# Debug mouse position
-def RGB(event, x, y, flags, param):
-    if event == cv2.EVENT_MOUSEMOVE:
-        print(f"Mouse moved to: [{x}, {y}]")
-
-cv2.namedWindow("RGB")
-cv2.setMouseCallback("RGB", RGB)
 
 
-line_y=304
+
 
 frame_count = 0
 hs={}
@@ -41,22 +32,15 @@ while True:
         for track_id, box, class_id in zip(ids, boxes, class_ids):
             x1, y1, x2, y2 = box
             label = names[class_id]
-            cx=int(x1+x2)//2
-            cy=int(y1+y2)//2
-            if track_id in hs:
-                prev_cx,prev_cy=hs[track_id]
-                if prev_cy<line_y<=cy:
-                  cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
-                  cv2.putText(frame, label, (x1 + 3, y1 - 7),
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
+            cv2.putText(frame, label, (x1 + 3, y1 - 7),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1)
-                  cv2.putText(frame, str(track_id), (x2 + 3, y2 - 7),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1)
-            hs[track_id]=(cx,cy)
+               
           
 
            
-    cv2.line(frame,(0,line_y),(frame.shape[1],line_y),(0,0,255),2)
-    cv2.imshow("RGB", frame)
+    
+    cv2.imshow("FRAME", frame)
     if cv2.waitKey(0) & 0xFF == 27:  # ESC to quit
         break
 
